962f08c1cae0eab1cb8214b95cb88c7d8de75e6c
Eztof <90273638+Eztof@users.noreply.github.com>
2025-07-20 11:04:27 +0000

    1

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,11 +5,10 @@ const firebaseConfig = {
   projectId: "lauriver-31a6f",
   storageBucket: "lauriver-31a6f.firebasestorage.app",
   messagingSenderId: "508140835438",
-  appId: "1:508140835438:web:4326ed6b40c01037e64c7f",
-  databaseURL: "https://lauriver-31a6f-default-rtdb.firebaseio.com/"
+  appId: "1:508140835438:web:4326ed6b40c01037e64c7f"
 };
 
 // Initialisieren
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-const db = firebase.database();
+const firestore = firebase.firestore();
